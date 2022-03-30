4ff269bd5799e2c885bcf4641df3981517d69e04
yodaheYT <94496398+yodaheYT@users.noreply.github.com>
2022-03-29 18:59:18 -0600

    First Commit

---
+++ server/cloud.js
@@ -0,0 +1,45 @@
+export function get(table) {
+    var tablee = [];
+    db.collection(table).get().then(snapshot => {
+        snapshot.docs.forEach(doc => {
+            tablee.push(doc.data());
+        });
+    })
+    return tablee;
+}
+export function set(table, doc) {
+    db.collection(table).add(doc);
+    return 'sent!';
+}
+export function live(table) {
+    var tablee = [];
+    db.collection(table).orderBy('date').onSnapshot(snapshot => {
+        let changes = snapshot.docChanges();
+        changes.forEach(change => {
+            if (change.type == 'added') {
+                render(change.doc);
+            } else if (change.type == 'removed') {
+                let li = mainDiv.querySelector('[data-id=' + "a" + change.doc.id + ']');
+                mainDiv.removeChild(li);
+            }
+        });
+    });
+    function render(doc) {
+        const mainDiv = document.getElementById('mainDiv');
+
+        let li = document.createElement('li');
+        let Sender = document.createElement('p');
+        let Content = document.createElement('p');
+    
+        li.setAttribute('data-id', "a" + doc.id);
+        li.setAttribute('class', 'field');
+    
+        Sender.textContent = "Sender: " + doc.data().sender + "\n";
+        Content.textContent = "Content: " + doc.data().content;
+    
+        li.appendChild(Sender);
+        li.appendChild(Content);
+    
+        mainDiv.appendChild(li);
+    }
+}
---
+++ server/main.js
@@ -0,0 +1,23 @@
+import { set, get, live } from './cloud.js';
+
+const form = document.getElementById('send');
+const form2 = document.getElementById('Name');
+
+window.onload = function() {
+    form.getElementsByTagName('input')[0].focus();
+    form.getElementsByTagName('input')[0].select();
+};
+
+live('messages');
+
+console.log(get('messages'));
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    form.message.value = '';
+    set('messages',{
+        sender: form2.name.value,
+        content: form.message.value,
+        date: new Date().getTime()
+    })
+});
